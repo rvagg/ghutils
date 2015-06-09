ebd8d05dcdd5d6dd543b742333ce64872e6bdd71
Rod Vagg <rod@vagg.org>
2015-06-09 22:42:46 +1000

    added test utilities

---
+++ test.js
@@ -0,0 +1,84 @@
+const http           = require('http')
+    , test           = require('tape')
+    , xtend          = require('xtend')
+    , EE             = require('events').EventEmitter
+    , bl             = require('bl')
+    , jsonist        = require('jsonist')
+    , _jsonistget    = jsonist.get
+    , _jsonistpost   = jsonist.post
+
+
+function makeServer (data) {
+  var ee     = new EE()
+    , i      = 0
+    , server = http.createServer(function (req, res) {
+        ee.emit('request', req)
+
+        var _data = Array.isArray(data) ? data[i++] : data
+        res.end(JSON.stringify(_data))
+
+        if (!Array.isArray(data) || i == data.length)
+          server.close()
+      })
+      .listen(0, function (err) {
+        if (err)
+          return ee.emit('error', err)
+
+        jsonist.get = function (url, opts, callback) {
+          ee.emit('get', url, opts)
+          return _jsonistget('http://localhost:' + server.address().port, opts, callback)
+        }
+
+        ee.emit('ready')
+      })
+      .on('close', ee.emit.bind(ee, 'close'))
+
+  return ee
+}
+
+
+function toAuth (auth) {
+  return 'Basic ' + (new Buffer(auth.user + ':' + auth.token).toString('base64'))
+}
+
+
+function verifyRequest (t, auth) {
+  return function (req) {
+    t.ok(true, 'got request')
+    t.equal(req.headers['authorization'], toAuth(auth), 'got auth header')
+  }
+}
+
+
+function verifyUrl (t, urls) {
+  var i = 0
+  return function (_url) {
+    if (i == urls.length)
+      return t.fail('too many urls/requests')
+    t.equal(_url, urls[i++], 'correct url')
+  }
+}
+
+
+function verifyClose (t) {
+  return function () {
+    t.ok(true, 'got close')
+  }
+}
+
+
+function verifyData (t, data) {
+  return function (err, _data) {
+    t.notOk(err, 'no error')
+    t.ok((data === '' && _data === '') || _data, 'got data')
+    t.deepEqual(_data, data, 'got expected data')
+  }
+}
+
+
+module.exports.makeServer    = makeServer
+module.exports.toAuth        = toAuth
+module.exports.verifyRequest = verifyRequest
+module.exports.verifyUrl     = verifyUrl
+module.exports.verifyClose   = verifyClose
+module.exports.verifyData    = verifyData
